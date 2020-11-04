d66c7553baba63d945bc590723f9c69925507221
matías <matiasmaldonado400@gmail.com>
2020-11-03 22:58:18 -0300

    files

---
+++ tarea.bat
@@ -0,0 +1,17 @@
+::
+@echo off 
+:a 
+start //pagina url//
+start //pagina url//
+start //pagina url//
+
+goto :a
+
+
+en mi compu correr esto
+@echo off 
+start //pagina url//
+start //pagina url//
+start //pagina url//
+
+exit
---
+++ tarea.vbs
@@ -0,0 +1,4 @@
+do 
+Mensaje=msgbox("error al intentar abrir el archivo" ,16, "mensaje")
+loop 
+'esto tiene un loop para cerrarlo solo ve a la barra de tareas y desactiva
